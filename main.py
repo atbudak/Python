0e0214e3afa33c78c3300f627163320fcd8e07cd
ahmetbudak <ahmet1.budak@gmail.com>
2021-07-27 23:14:15 +0300

    Assignment

--- main.py
+++ main.py
@@ -88,3 +88,20 @@ else:
     print(is_Armstrong)
 # End
 
+# Prime number
+# Start
+num = int(input("Enter a number: "))
+flag = False
+
+if num > 1:
+    for i in range(2, num):
+        if (num % i) == 0:
+            flag = True
+            break
+
+if flag:
+    print(num, "is not a prime number")
+else:
+    print(num, "is a prime number")
+# End
+
